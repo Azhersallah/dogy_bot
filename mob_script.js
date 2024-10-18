7278e2c02d51a5843c0fdaa03d7413df3472b532
Azhersallah <70579615+Azhersallah@users.noreply.github.com>
2024-10-18 11:17:18 +0300

    Add files via upload

---
+++ mob_script.js
@@ -0,0 +1,15 @@
+
+function isLaptop() {
+    return !/Mobi|Android/i.test(navigator.userAgent);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (isLaptop()) {
+        document.body.innerHTML = '<h1 class="lap_text">This is Only Work on Mobile device!</h1><img id="qrCode" src="qr.png" alt="QR Code" /> <!-- Replace with the path to your QR code image -->'; // Clear all content
+        const qrCode = document.getElementById("qrCode");
+        qrCode.style.display = "block"; // Show QR code
+        document.body.appendChild(qrCode); // Add QR code to body
+    } else {
+        // Optional: You can add mobile-specific content here
+    }
+});
